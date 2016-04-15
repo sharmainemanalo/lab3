b2fb1a47e15eeca712bd9fad16fe702b9b5a6495
sharmaine <hello@sharmaine.me>
2016-04-15 11:45:12 -0700

    added files

---
+++ app/src/androidTest/java/tests/JUnit_test.java
@@ -0,0 +1,25 @@
+package tests;
+
+import android.test.ActivityInstrumentationTestCase2;
+import com.example.sharmaine.lab3.MainActivity;
+import com.example.sharmaine.lab3.R;
+
+/**
+ * Created by sharmaine on 4/15/16.
+ */
+public class JUnit_test extends ActivityInstrumentationTestCase2<MainActivity> {
+    MainActivity mainActivity;
+    public JUnit_test() {
+        super(MainActivity.class);
+    }
+
+    public void test_first() {
+        mainActivity = getActivity();
+        TextView textView = (TextView) mainActivity.findViewById(R.id.textView1);
+        
+        String tester = textView.getText().toString();
+        assertEquals("Bye World", tester);
+    }
+
+}
+
